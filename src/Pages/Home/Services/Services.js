834b530dc738c04ac40b77c437b94593259bc5a0
Nahidjc <nahidjc80@gmail.com>
2021-10-29 17:09:21 +0600

    add django api

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
+import "./Services.css";
+import { baseURL, headers } from "./menu.service";
+import Axios from "axios";
+const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    const getpost = () => {
+      Axios({
+        url: `${baseURL}/services/`,
+        headers: headers,
+        method: "GET",
+      })
+        .then((res) => {
+          console.log(res.data);
+          setServices(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getpost();
+  }, []);
+
+  return (
+    <div id="services">
+      <h2 className="text-primary mt-5">Our services</h2>
+      <div className="service-container">
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA04CE7kzdvFqCblS1OY1g5L1XdZgvrY-4",
+  authDomain: "genius-car-mecanics-439e2.firebaseapp.com",
+  projectId: "genius-car-mecanics-439e2",
+  storageBucket: "genius-car-mecanics-439e2.appspot.com",
+  messagingSenderId: "807349601827",
+  appId: "1:807349601827:web:f03ab357d1f287d3b29396",
+};
+
+export default firebaseConfig;
