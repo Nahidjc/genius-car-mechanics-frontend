aaf18fa5f59d0966aa8eb2a78065187a1073274f
Nahidjc <nahidjc80@gmail.com>
2021-10-30 15:36:26 +0600

    update successfully

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -13,6 +13,53 @@ const Booking = () => {
   const [price, setPrice] = useState(0);
   const [imgUrl, setImgUrl] = useState("");
   const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, price, imgUrl, description);
+    e.target.reset();
+    updateService();
+    handleClose();
+  };
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handlePrice = (e) => {
+    setPrice(e.target.value);
+  };
+  const handleImgUrl = (e) => {
+    setImgUrl(e.target.value);
+  };
+  const handleDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const updateService = () => {
+    console.log(name, description);
+    Axios({
+      url: `${baseURL}/service/${serviceId}/`,
+      method: "PUT",
+      headers: headers,
+      data: {
+        name: name,
+        description: description,
+        price: price,
+        imgUrl: imgUrl,
+      },
+    })
+      .then((response) => {
+        setName("");
+        setPrice(0);
+        setImgUrl("");
+        setDescription("");
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
   const removeService = () => {
     Axios({
       url: `${baseURL}/delete-service/${serviceId}/`,
@@ -72,16 +119,77 @@ const Booking = () => {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Edit {name} Details</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+            <div className="form-outline mb-4">
+              <input
+                onChange={handleName}
+                type="text"
+                name="name"
+                id="form4Example1"
+                value={name}
+                className="form-control border"
+              />
+              <label className="form-label" htmlFor="form4Example1">
+                Name
+              </label>
+            </div>
+
+            <div className="form-outline mb-4">
+              <input
+                onChange={handlePrice}
+                type="number"
+                name="price"
+                value={price}
+                id="form4Example2"
+                className="border form-control"
+              />
+              <label className="form-label" htmlFor="form4Example2">
+                Price
+              </label>
+            </div>
+            <div className="form-outline mb-4">
+              <input
+                type="text"
+                onChange={handleImgUrl}
+                value={imgUrl}
+                id="form4Example2"
+                name="imgUrl"
+                className="border form-control"
+              />
+              <label className="form-label" htmlFor="form4Example2">
+                Image Url
+              </label>
+            </div>
+
+            <div className="form-outline mb-4">
+              <textarea
+                className="border form-control"
+                id="form4Example3"
+                rows="4"
+                onChange={handleDescription}
+                value={description}
+                name="description"
+              ></textarea>
+              <label className="form-label" htmlFor="form4Example3">
+                Description
+              </label>
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-block mb-4">
+              Updated
+            </button>
+          </form>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          {/* <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
             Save Changes
-          </Button>
+          </Button> */}
         </Modal.Footer>
       </Modal>
     </div>
